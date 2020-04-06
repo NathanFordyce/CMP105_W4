94e456b370363d21b3eba82b6acda4b275da7a34
NathanFordyce <59959678+NathanFordyce@users.noreply.github.com>
2020-04-06 18:34:16 +0100

    week 4

--- Week4/CMP105App/Background.cpp
+++ Week4/CMP105App/Background.cpp
@@ -15,15 +15,12 @@ void Background::handleInput(float dt)
 {
 	view = window->getView();
 
-	if (window->getView() < sf::Vector2f(5, 0))
+	if (input->isKeyDown(sf::Keyboard::D))
 	{
-		if (input->isKeyDown(sf::Keyboard::D))
-		{
-			view.move(1.0f, 0);
-		}
+		view.move(1.0f, 0);
 	}
 	
-	if (input->isKeyDown(sf::Keyboard::A))
+	else if (input->isKeyDown(sf::Keyboard::A))
 	{
 		view.move(-1.0f, 0);
 	}
